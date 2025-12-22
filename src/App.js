ded6b5383fa09c6ccbb12ee8dc2d07f29fda64ed
Hein <hein@speccon.co.za>
2025-12-22 15:26:35 +0200

    Add setup route for admin initialization

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-d
 import { AuthProvider } from './context/AuthContext';
 import Login from './components/Login';
 import Dashboard from './components/Dashboard';
+import AdminSetup from './components/AdminSetup';
 import PrivateRoute from './components/PrivateRoute';
 import './App.css';
 
@@ -12,6 +13,14 @@ function App() {
       <Router>
         <Routes>
           <Route path="/login" element={<Login />} />
+          <Route
+            path="/setup"
+            element={
+              <PrivateRoute>
+                <AdminSetup />
+              </PrivateRoute>
+            }
+          />
           <Route
             path="/dashboard"
             element={
